464ca954b631016156cce960f2264f399733ed3f
akihokurino <aki030402@gmail.com>
2022-01-27 01:45:34 +0900

    fixed

--- lib/flow/account.dart
+++ lib/flow/account.dart
@@ -1,8 +1,11 @@
+import 'dart:typed_data';
+
 import 'package:flutter_flow_wallet/flow/generated/entities/account.pb.dart';
 import 'package:rlp/rlp.dart';
 
 extension AccountKeyEncorder on AccountKey {
   List<int> rlpEncode() {
-    return Rlp.encode([publicKey, signAlgo, hashAlgo, weight]);
+    final publicKeyData = Uint8List.fromList(publicKey);
+    return Rlp.encode([publicKeyData, signAlgo, hashAlgo, weight]);
   }
 }
